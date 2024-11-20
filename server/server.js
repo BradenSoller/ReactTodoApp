e89c8092791b4a31fb0305018809b3a257403f40
Braden Soller <braden.soller18@gmail.com>
2024-11-20 17:14:47 -0600

    added todo route to server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route Includes
 const userRouter = require('./routes/user.router');
+const todoRouter = require('./routes/todo.router');
 
 // Express Middleware
 app.use(express.json());
@@ -24,6 +25,8 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+app.use('/api/todo', todoRouter);
+
 
 // Listen Server & Port
 app.listen(PORT, () => {
