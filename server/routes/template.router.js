b48fcd51ac23591ce427f697aef6a3bb8b88bb46
Braden Soller <braden.soller18@gmail.com>
2024-11-20 17:10:46 -0600

    added get,post, and put route

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -2,18 +2,60 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
 router.get('/', (req, res) => {
-  // GET route code here
+  const getTodoList = `SELECT * FROM "todos"`
+  pool.query(getTodoList)
+  .then(result => {
+    console.log("results.rows", result.rows);
+    res.send(result.rows);
+   
+  })
+  .catch((err) => {
+    console.log("ERROR: Get all appointments", err);
+    res.sendStatus(500);
+  });
+
+})
+
+router.post('/', (req, res) => {
+  const query = `
+  INSERT INTO "todos" ("text")
+    VALUES ($1);
+  `
+  const values = [
+    req.body.text
+  ]
+
+  console.log("dssdcsdc")
+  pool
+    .query(query, values)
+    .then(result => {
+      res.sendStatus(201)
+    })
+    .catch((err) => {
+      console.error('POST route failed:', err)
+      res.sendStatus(500)
+    })
+   
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+
+router.put('/:id', (req, res) => {
+
+const sqlText = `
+UPDATE "todos"
+ SET "isComplete" = NOT "isComplete"
+ WHERE "id" = ${req.params.id};
+  `
+
+pool.query(sqlText)
+.then((dbResult) =>{
+    res.sendStatus(200);
+})
+.catch((dbError)=>{
+    console.log('PUT /status:id failed', dbError)
+    res.sendStatus(500);
+})
 });
 
 module.exports = router;
